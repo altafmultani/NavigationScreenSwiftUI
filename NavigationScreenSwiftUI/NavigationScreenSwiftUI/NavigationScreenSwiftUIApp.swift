61ba775afb9709bb005d5fb638c0b56e10ac0b2e
Altaf Multani <altafmultani16@gmail.com>
2022-09-20 14:14:02 +0530

    first commit

---
+++ NavigationScreenSwiftUI/NavigationScreenSwiftUI/NavigationScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationScreenSwiftUIApp.swift
+//  NavigationScreenSwiftUI
+//
+//  Created by Javid Multani on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
